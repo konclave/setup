fcb46997d31377fc10c29e062920873e45cc1311
Ivan Vasilev <875116+konclave@users.noreply.github.com>
2026-02-03 21:22:51 +0100

    Update macos-prepare.sh with monolisa

--- macos-prepare.sh
+++ macos-prepare.sh
@@ -34,6 +34,24 @@ defaults write "Apple Global Domain" AppleKeyboardUIMode 2 \
     && echo '✅ Keyboard navigation is set up' \
     || echo '❗️ Could not set up keyboard navigation';
 
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-Black.ttf -s -o ~/Library/Fonts/MonoLisa-Black.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-BlackItalic.ttf -s -o ~/Library/Fonts/MonoLisa-BlackItalic.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-Bold.ttf -s -o ~/Library/Fonts/MonoLisa-Bold.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-BoldItalic.ttf -s -o ~/Library/Fonts/MonoLisa-BoldItalic.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-ExtraLight.ttf -s -o ~/Library/Fonts/MonoLisa-ExtraLight.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-ExtraLightItalic.ttf -s -o ~/Library/Fonts/MonoLisa-ExtraLightItalic.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-Light.ttf -s -o ~/Library/Fonts/MonoLisa-Light.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-LightItalic.ttf -s -o ~/Library/Fonts/MonoLisa-LightItalic.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-Medium.ttf -s -o ~/Library/Fonts/MonoLisa-Medium.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-MediumItalic.ttf -s -o ~/Library/Fonts/MonoLisa-MediumItalic.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-Regular.ttf -s -o ~/Library/Fonts/MonoLisa-Regular.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-RegularItalic.ttf -s -o ~/Library/Fonts/MonoLisa-RegularItalic.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-Thin.ttf -s -o ~/Library/Fonts/MonoLisa-Thin.ttf && \
+curl https://raw.githubusercontent.com/okonet/Monolisa/refs/heads/master/MonoLisa-ThinItalic.ttf -s -o ~/Library/Fonts/MonoLisa-ThinItalic.ttf && \
+echo '✅ Monolisa font downloaded' \
+|| echo '❗️ Could not download Monolisa font';
+
+
 sudo mdutil -i off -a \
 	&& sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist \
 	&& echo '✅ spotlight index turn off' \
@@ -43,8 +61,6 @@ sudo mdutil -i off -a \
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \
     && brew install font-fira-code \
     && brew install iterm2 \
-#    && brew install keepingyouawake \
-    && brew install --cask rectangle
     && brew install only-switch \
     && brew install boop \
     && brew install bluesnooze \
